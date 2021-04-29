41693cb1f34f1c1ca6d0417e8158a38cee690d04
Allison Branch <aab4ad@virginia.edu>
2021-04-29 18:12:09 -0400

    db connection

---
+++ connectdb.php
@@ -0,0 +1,48 @@
+<?php
+
+/******************************/
+// connecting to GCP cloud SQL instance
+
+// $username = 'root';
+// $password = 'your-root-password';
+
+// $dbname = 'your-database-name';
+
+// if PHP is on GCP standard App Engine, use instance name to connect
+// $host = 'instance-connection-name';
+
+// if PHP is hosted somewhere else (non-GCP), use public IP address to connect
+// $host = "public-IP-address-to-cloud-instance";
+
+
+/******************************/
+// connecting to DB on XAMPP (local)
+
+$username = 'WepAppProject';
+$password = 'GoodF!lms1358';
+$host = 'localhost:3307';
+$dbname = 'WepAppProject';
+
+
+/******************************/
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+/** connect to the database **/
+try 
+{
+   $db = new PDO($dsn, $username, $password);   
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, 
+   // use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+?>
--- login.php
+++ login.php
@@ -22,14 +22,14 @@ $_SESSION['user'] = "";
     <div class="main-page-area" id="login-box">
       <img id="login-logo" src="logo3.png"></img>
       <h1 class="page-title" id="login-title"> Hospital Portal Login </h1>
-      <form>
+      <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
         <div class="form-group text-center">
           <!-- <label class="login-label" for="inputUser">Username</label> -->
-          <input type="username" class="form-control login-input" id="inputUser" placeholder="Username" required>
+          <input name='username' type="username" class="form-control login-input" id="inputUser" placeholder="Username" required>
         </div>
         <div class="form-group text-center">
           <!-- <label class="login-label" for="inputPassword">Password</label> -->
-          <input type="password" class="form-control login-input" id="inputPassword" placeholder="Password" required>
+          <input name='password' type="password" class="form-control login-input" id="inputPassword" placeholder="Password" required>
         </div>
         <div class="text-center">
           <button id="login-button" type="submit" class="btn btn-primary text-center">Log In</button>
@@ -38,23 +38,14 @@ $_SESSION['user'] = "";
     </div>
   </body>
 
-  <?php
-    require_once('./library.php');
-    $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
-    // Check connection
-    if (mysqli_connect_errno()) {
-      echo("Can't connect to MySQL Server. Error code: " .
-      mysqli_connect_error());
-      return null;
-    }
-?>
 
-  <!-- <?php 
+  <?php 
     // checks that a user with the given username and password exists, and sets the session variables -->
     require('connectdb.php');
     if ($_SERVER['REQUEST_METHOD'] == "POST" && strlen($_POST['username']) > 0){
       $user = trim($_POST['username']);
       $pwd = md5(trim($_POST['password']));
+      echo $user . $pwd;
 
       # Check that the username and password combo are correct (that they exist in the users table)
       global $db;
@@ -66,16 +57,17 @@ $_SESSION['user'] = "";
       $results = $statement->fetchAll();
       $statement->closecursor();
       if (count($results) > 0){
+        echo
         $_SESSION['user'] = $user; 
         $_SESSION['toggle']=false;
         echo $_SESSION['user'];
-        header('Location: myprofile.php');
+        header('Location: appts.php');
       }
       else{ //username and password not found
         echo "<script>  showErrorBox(); </script>";
       }
     }
-  ?> -->
+  ?>
 
   <!-- Bootstrap Javascript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
